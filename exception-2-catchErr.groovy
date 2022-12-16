613abb0574dc5defe25b9a5e395e72f4be7fdfdf
Marek Kaminski <mkaminski2@sente.pl>
2022-12-16 11:13:36 +0100

    part_2

---
+++ exception-2-catchErr.groovy
@@ -0,0 +1,32 @@
+pipeline{
+    agent 
+    {
+        node 'slave01'
+    }
+    stages{
+        stage("Pull image 1")
+        {
+            steps{
+                script{
+                    def image = docker.image("python:3.6.12-alpine")
+                    image.pull()
+                }
+
+            }
+        }
+
+         stage("Pull image 2")
+        {
+            steps{
+               catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE' ) {
+                        def image = docker.image("python:3.6.1111-alpine")
+                        image.pull()
+                    }
+                }
+            }
+            
+        }
+
+    }
+
+}
